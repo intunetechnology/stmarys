4d27e76efb6715ca9523d3d859598ce8928a70cd
Zachary Snyder <contact@zsny.dev>
2021-08-01 18:42:41 -0400

    update

--- map_user_share.bat
+++ map_user_share.bat
@@ -1,13 +1,15 @@
-set STAFF="%USERNAME%"
-del "%userprofile%\Desktop\%STAFF%.lnk"
+@echo off
+del "%userprofile%\Desktop\%USERNAME%.lnk"
 
 set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
 
 echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
-echo sLinkFile = "%userprofile%\Desktop\%STAFF%.lnk" >> %SCRIPT%
+echo sLinkFile = "%userprofile%\Desktop\%USERNAME%.lnk" >> %SCRIPT%
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
-echo oLink.TargetPath = "\\STMARYS-SERVER\Home\%STAFF%" >> %SCRIPT%
+echo oLink.TargetPath = "\\STMARYS-SERVER\Home\%USERNAME%" >> %SCRIPT%
 echo oLink.Save >> %SCRIPT%
 
 cscript /nologo %SCRIPT%
 del %SCRIPT%
+
+Exit
